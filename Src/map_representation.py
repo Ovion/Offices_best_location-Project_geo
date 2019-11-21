518a3ba6a7a26e2d3d49f3609346890682549124
Ovi <jm.fdez.corbacho@gmail.com>
2019-11-21 13:01:50 +0100

    Al baño

--- Src/map_representation.py
+++ Src/map_representation.py
@@ -3,4 +3,10 @@ import folium
 
 
 def print_map(df):
+    max_rank_office = df.iloc[df['total_rank'].idxmax()]['offices']
+    lon = max_rank_office['longitude']
+    lat = max_rank_office['latitude']
+    city = max_rank_office['city']
+
+    map_city = folium.Map(location=[lat, lon], zoom_start=12)
     return 0
