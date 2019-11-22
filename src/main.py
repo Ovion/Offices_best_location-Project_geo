42a1c4bf9ac3c0cfa61e6db7d647fbb0d9b73b8e
Ovi <jm.fdez.corbacho@gmail.com>
2019-11-22 15:06:21 +0100

    Añadidos comentarios

--- src/main.py
+++ src/main.py
@@ -13,8 +13,7 @@ import collections_mdb.tech_comps as tc
 
 
 def main():
-    # Display menu start
-    # Display load data
+    # Primero pido al usuario si quiere o no cargar la base de datos
     options = ['y', 'n']
     yes_or_no = str(input('Do you need to load data? [y/n]: '))
     while yes_or_no not in options:
@@ -29,6 +28,7 @@ def main():
         tc.load_tech()
 
     menu.display_M1()
+    # En este apartado pido la primera preferencia
     pref_1 = str(input('Enter your first preference by the number: '))
     lst_pref = ['1', '2', '3']
     while pref_1 not in lst_pref:
@@ -40,11 +40,15 @@ def main():
     companies = comps_coll.find()
     df_comps = pd.DataFrame(companies)
     print('Getting all locations')
+
+    # Consigo todas las localizaciones para luego hacer geoquerys con ellas
     lst_loc = [df_comps.location[e] for e in range(len(df_comps))]
 
+    # En función de lo que haya elegido el usuario escojo una determinada query
     query1 = vble.get_variables_perefence(pref_1)
     df_comps = fm.rank(comps_coll, df_comps, query1, lst_loc, 1)
 
+    # Lo mismo pero para la segunda elección
     menu.display_M2()
     pref_2 = str(input('Enter your second preference by the number: '))
     while (pref_2 not in lst_pref) and (pref_2 == pref_1):
@@ -63,15 +67,20 @@ def main():
         if str(i) not in prev_q:
             pref_3 = str(i)
     query3 = vble.get_variables_perefence(pref_3)
+
+    # Calculo para cada query una columna con su propio rank
     df_comps = fm.rank(comps_coll, df_comps, query3, lst_loc, 3)
 
-    # total rank
+    # calculo el rango total como una ponderación de los ranks anteriores
     df_comps['total_rank'] = df_comps.iloc[:, -3]*0.6 + \
         df_comps.iloc[:, -2]*0.3 + df_comps.iloc[:, -1]*0.1
 
 # -------
 
+    # Ordeno todas las compañías con el fin de escoger la que mayor rank tenga
     df_comps = df_comps.sort_values(by=['total_rank'], ascending=False)
+
+    # Meto en una lista el top 3 ciudades (Preguntadme)
     lst_cities = []
     for e in df_comps.offices:
         if e['city'] not in lst_cities:
@@ -79,6 +88,7 @@ def main():
             if len(lst_cities) == 3:
                 break
 
+    # Pido al usuario que me diga en qué ciudad quiere su oficina
     menu.display_Mcity(lst_cities)
 
     the_city = str(input('Enter THE city: '))
@@ -87,6 +97,7 @@ def main():
         if the_city in lst_cities:
             break
 
+    # Elijo de la ciudad que ha elegido el usuario, la oficina con mayor rank
     max_rank_office = {}
     for e in df_comps.offices:
         if e['city'] == the_city:
@@ -95,6 +106,7 @@ def main():
     lon_city = max_rank_office['longitude']
     lat_city = max_rank_office['latitude']
 
+    # Pinto el mapa
     mp.print_map(the_city, lon_city, lat_city)
 
 
